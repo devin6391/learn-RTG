f50ba07a32c6b84852332cae5976c2711ac1d52b
Vineet Dev <vineetdev2008@gmail.com>
2018-07-09 01:00:33 +0530

    basic carousel done

--- src/packages/carousel/carousel.style.js
+++ src/packages/carousel/carousel.style.js
@@ -7,11 +7,13 @@ export const appBarStyles = theme => ({
 
 export const carouselElemWidth = 345;
 export const carouselElemMargin = 33;
+const carouselTransitionTime = "0.3s";
 const carouselPadding = 10;
 const carouselArrowStyle = theme => ({
   flex: 1,
   display: "flex",
   backgroundColor: theme.palette.grey[300],
+  cursor: "pointer",
   "& > svg": {
     margin: "auto"
   },
@@ -49,11 +51,37 @@ export const carouselComponentStyles = theme => ({
   itemList: {
     width: "auto",
     display: "flex",
-    transition: "0.3s",
+    transition: carouselTransitionTime,
     "& > div": {
       width: carouselElemWidth,
       paddingTop: carouselPadding,
       paddingBottom: carouselPadding
     }
+  },
+  rtgList: {
+    width: carouselElemWidth,
+    display: "flex",
+    position: "relative",
+    height: carouselElemWidth - 65
+  },
+  rtgWrapper: {
+    position: "absolute",
+    top: carouselPadding,
+    bottom: carouselPadding,
+    left: 0,
+    right: 0,
+    width: carouselElemWidth
+  },
+  rtgFarRight: {
+    transform: `translate3d(${carouselElemWidth + carouselElemMargin}px, 0, 0)`
+  },
+  rtgFarLeft: {
+    transform: `translate3d(-${carouselElemWidth + carouselElemMargin}px, 0, 0)`
+  },
+  rtgCenter: {
+    transform: `translate3d(${carouselElemMargin}px, 0, 0)`
+  },
+  rtgWithTransition: {
+    transition: carouselTransitionTime
   }
 });
--- src/packages/carousel/rtgCarouselContainer/index.js
+++ src/packages/carousel/rtgCarouselContainer/index.js
@@ -1,33 +1,108 @@
-import React from "react";
+import React, { Component } from "react";
 import { withStyles } from "@material-ui/core/styles";
+import { Transition } from "react-transition-group";
 import CarouselComponent from "../carouselComponent";
 import { carouselComponentStyles } from "../carousel.style";
 import KeyboardLeftIcon from "@material-ui/icons/KeyboardArrowLeft";
 import KeyboardRightIcon from "@material-ui/icons/KeyboardArrowRight";
 
-const CarouselContainer = ({ classes, dataArr }) => (
-  <div className={classes.root}>
-    <div className={classes.leftMove}>
-      <KeyboardLeftIcon />
-    </div>
-    <div className={classes.carouselContainer}>
-      <div className={classes.itemList}>
-        {dataArr.map(data => (
-          <CarouselComponent
-            key={data.title}
-            imageUrl={data.imageUrl}
-            title={data.title}
-            text={data.text}
-            classes={classes}
-          />
-        ))}
+class CarouselContainer extends Component {
+  state = {
+    currIndex: 0,
+    direction: 0 // 0 for left and 1 for right
+  };
+
+  slideLeftClick = () => {
+    let { currIndex } = this.state;
+    if (currIndex > 0) {
+      currIndex--;
+    } else if (currIndex === 0) {
+      currIndex = this.props.dataArr.length - 1;
+    } else {
+      throw Error(`Current Index of element can't go below 0`);
+    }
+    this.setState({ currIndex, direction: 1 });
+  };
+
+  slideRightClick = () => {
+    let { currIndex } = this.state;
+    let maxIndex = this.props.dataArr.length - 1;
+    if (currIndex < maxIndex) {
+      currIndex++;
+    } else if (currIndex === maxIndex) {
+      currIndex = 0;
+    } else {
+      throw Error(`Current Index of element can't go beyond ${maxIndex}`);
+    }
+    this.setState({ currIndex, direction: 0 });
+  };
+
+  render() {
+    const { classes, dataArr } = this.props;
+    const { direction, currIndex } = this.state;
+    return (
+      <div className={classes.root}>
+        <div className={classes.leftMove} onClick={this.slideLeftClick}>
+          <KeyboardLeftIcon />
+        </div>
+        <div className={classes.carouselContainer}>
+          <div className={classes.rtgList}>
+            {dataArr.map((data, index) => (
+              <Transition in={index === currIndex} key={data.title} timeout={1}>
+                {state => {
+                  console.log(
+                    `${
+                      data.title
+                    } is in state - ${state}. Its index is ${index} whereas currIndex is ${currIndex}`
+                  );
+                  let wrapperClass = classes.rtgFarRight;
+                  switch (state) {
+                    case "entering":
+                      wrapperClass = !!direction
+                        ? classes.rtgFarLeft
+                        : classes.rtgFarRight;
+                      break;
+                    case "entered":
+                      wrapperClass =
+                        classes.rtgCenter + " " + classes.rtgWithTransition;
+                      break;
+                    case "exiting":
+                      wrapperClass =
+                        classes.rtgCenter + " " + classes.rtgWithTransition;
+                      break;
+                    case "exited":
+                      wrapperClass =
+                        (!!direction
+                          ? classes.rtgFarRight
+                          : classes.rtgFarLeft) +
+                        " " +
+                        classes.rtgWithTransition;
+                      break;
+                    default:
+                      throw new Error("Transition has no state");
+                  }
+                  return (
+                    <div className={`${wrapperClass} ${classes.rtgWrapper}`}>
+                      <CarouselComponent
+                        imageUrl={data.imageUrl}
+                        title={data.title}
+                        text={data.text}
+                        classes={classes}
+                      />
+                    </div>
+                  );
+                }}
+              </Transition>
+            ))}
+          </div>
+        </div>
+        <div className={classes.rightMove} onClick={this.slideRightClick}>
+          <KeyboardRightIcon />
+        </div>
       </div>
-    </div>
-    <div className={classes.rightMove}>
-      <KeyboardRightIcon />
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default withStyles(carouselComponentStyles, { withTheme: true })(
   CarouselContainer
